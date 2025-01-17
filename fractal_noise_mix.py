f0881ceff6f3ae09a66ab8a0cf8827d9f9a6c8cd
momiji-fullmoon <shinohara.t.af@gmail.com>
2025-01-17 14:03:59 +0900

    Update fractal_noise_mix.py

--- fractal_noise_mix.py
+++ fractal_noise_mix.py
@@ -1,71 +1,198 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import argparse
 import numpy as np
-import open3d as o3d
-import matplotlib.pyplot as plt
 
-def mix_point_clouds_with_colors(pc1, pc2, ratio=0.5):
-    """
-    Mix two point clouds with colors to differentiate their origin.
-    :param pc1: First point cloud (N x 3 numpy array)
-    :param pc2: Second point cloud (M x 3 numpy array)
-    :param ratio: Ratio of points from the first point cloud in the mixed result
-    :return: Mixed point cloud with labels (P x 3 numpy array, P x 1 array for labels)
-    """
+import open3d
+import random
 
-    # Calculate the number of points to sample from each point cloud.
-    num_pc1 = int(len(pc1) * ratio)
-    # Change: Ensure num_pc2 is not larger than the size of pc2
-    num_pc2 = int(len(pc2) * (1-ratio))  
-    # Previously: num_pc2 = len(pc1) + len(pc2) - num_pc1
-    # This would make num_pc2 larger than len(pc2) if ratio<0.5
-    # Randomly sample points from pc1 and pc2
-    pc1_sampled = pc1[np.random.choice(pc1.shape[0], num_pc1, replace=False)]
-    pc2_sampled = pc2[np.random.choice(pc2.shape[0], num_pc2, replace=False)]
+import open3d
+import numpy as np
+import os
+import random
+import warnings
+warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
 
-    # Combine points and labels
-    mixed_points = np.vstack([pc1_sampled, pc2_sampled])
-    labels = np.hstack([np.zeros(num_pc1), np.ones(num_pc2)])  # 0: pc1, 1: pc2
+class ifs_function():
+    def __init__(self):
+        self.prev_x,self.prev_y, self.prev_z = 0.0, 0.0, 0.0
+        self.function  = []
+        self.xs,self.ys, self.zs = [],[],[]
+        self.select_function = []
+        self.temp_proba = 0.0
 
-    return mixed_points, labels
+    def set_param(self,a,b,c,d,e,f,g,h,i,j,k,l,proba, **kwargs):
+        if "weight_a" in kwargs:
+            a *= kwargs["weight_a"]
+        if "weight_b" in kwargs:
+            b *= kwargs["weight_b"]
+        if "weight_c" in kwargs:
+            c *= kwargs["weight_c"]
+        if "weight_d" in kwargs:
+            d *= kwargs["weight_d"]
+        if "weight_e" in kwargs:
+            e *= kwargs["weight_e"]
+        if "weight_f" in kwargs:
+            f *= kwargs["weight_f"]
+        if "weight_g" in kwargs:
+            g *= kwargs["weight_g"]
+        if "weight_h" in kwargs:
+            h *= kwargs["weight_h"]
+        if "weight_i" in kwargs:
+            i *= kwargs["weight_i"]
+        if "weight_j" in kwargs:
+            j *= kwargs["weight_j"]
+        if "weight_k" in kwargs:
+            k *= kwargs["weight_k"]
+        if "weight_l" in kwargs:
+            l *= kwargs["weight_l"]
+        temp_function  = {"a":a,"b":b,"c":c,"d":d,"e":e,"f":f,"g":g,"h":h,"i":i,"j":j,"k":k,"l":l,"proba":proba}
+        self.function.append(temp_function)
+        self.temp_proba += proba
+        self.select_function.append(self.temp_proba)
 
-def visualize_mixed_point_cloud_matplotlib(mixed_pc, labels):
-    """
-    Visualize the mixed point cloud with Matplotlib.
-    :param mixed_pc: Mixed point cloud (P x 3 numpy array)
-    :param labels: Labels indicating the origin of each point (P x 1 numpy array)
-    """
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
 
-    # Separate points by labels
-    pc1_points = mixed_pc[labels == 0]
-    pc2_points = mixed_pc[labels == 1]
+    def calculate(self,iteration):
+        """ Recursively calculate coordinates for args.iteration """
+        rand = np.random.random(iteration)
+        select_function = self.select_function
+        function = self.function
+        prev_x, prev_y, prev_z = self.prev_x, self.prev_y, self.prev_z
+        for i in range(iteration-1):
+            for j in range(len(select_function)):
+                if rand[i] <= select_function[j]:
+                    next_x = prev_x*function[j]["a"] + \
+                            prev_y*function[j]["b"] + \
+                            prev_z*function[j]["c"] + \
+                            function[j]["j"]
+                    next_y = prev_x*function[j]["d"] + \
+                            prev_y*function[j]["e"] + \
+                            prev_z*function[j]["f"] + \
+                            function[j]["k"]
+                    next_z = prev_x*function[j]["g"] + \
+                            prev_y*function[j]["h"] + \
+                            prev_z*function[j]["i"] + \
+                            function[j]["l"]
+                    break
+            self.xs.append(next_x), self.ys.append(next_y), self.zs.append(next_z)
+            prev_x, prev_y, prev_z = next_x, next_y, next_z
+        point_data = np.array((self.xs,self.ys,self.zs), dtype = float)
+        return point_data
 
-    # Plot point cloud 1 (red) and point cloud 2 (green)
-    ax.scatter(pc1_points[:, 0], pc1_points[:, 1], pc1_points[:, 2], c='r', label='Point Cloud 1', alpha=0.6)
-    ax.scatter(pc2_points[:, 0], pc2_points[:, 1], pc2_points[:, 2], c='g', label='Point Cloud 2', alpha=0.6)
+def conf():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--load_root", default="./PC-FractalDB/3DIFS_param", type = str, help="load csv root")
+	parser.add_argument("--save_root", default="./PC-FractalDB/3Dfractalmodel", type = str, help="save PLY root")
+	parser.add_argument("--iteration", default=10000, type = int)
+	parser.add_argument("--numof_classes", default=1000, type = int)
+	parser.add_argument("--start_class", default=0, type = int)
+	parser.add_argument("--numof_instance", default=1000, type = int)
+	parser.add_argument("--normalize", default=1.0, type=float)
+	parser.add_argument("--ratio", default=0.8, type=float)
+	parser.add_argument('--visualize', action='store_true')
+	args = parser.parse_args()
+	return args
 
-    ax.set_title("Mixed Point Clouds (Colored by Origin)")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    ax.legend()
-    plt.show()
+def min_max(args, x, axis=None):
+    min = x.min(axis=axis, keepdims=True)
+    max = x.max(axis=axis, keepdims=True)
+    result = ((x-min)/(max-min)) * (args.normalize - (-args.normalize)) - args.normalize
+    return result
 
-# Example usage
+def centoroid(point):
+	new_centor = []
+	sum_x = (sum(point[0]) / args.iteration)
+	sum_y = (sum(point[1]) / args.iteration)
+	sum_z = (sum(point[2]) / args.iteration)
+	centor_of_gravity = [sum_x, sum_y, sum_z]
+	fractal_point_x = (point[0] - centor_of_gravity[0]).tolist()
+	fractal_point_y = (point[1] - centor_of_gravity[1]).tolist()
+	fractal_point_z = (point[2] - centor_of_gravity[2]).tolist()
+	new_centor.append(fractal_point_x)
+	new_centor.append(fractal_point_y)
+	new_centor.append(fractal_point_z)
+	new = np.array(new_centor)
+	return new
+
 if __name__ == "__main__":
-    # Load point clouds from PLY files
-    pc1_file = "point1.ply"
-    pc2_file = "point2.ply"
+	starttime = time.time()
+	args = conf()
 
-    pc1_o3d = o3d.io.read_point_cloud(pc1_file)
-    pc2_o3d = o3d.io.read_point_cloud(pc2_file)
+	# object_list = ['object', 'main', 'mix']
+	# for i in object_list:
+	os.makedirs(args.save_root, exist_ok=True)
 
-    # Convert Open3D point clouds to numpy arrays
-    pc1 = np.asarray(pc1_o3d.points)
-    pc2 = np.asarray(pc2_o3d.points)
+	csv_names = os.listdir(args.load_root)
+	csv_names.sort()
+	mix_csv_names = os.listdir(args.load_root)
+	mix_csv_names = random.sample(mix_csv_names, k=args.numof_instance)
 
-    # Mix the point clouds with a 50-50 ratio and assign labels
-    mixed_pc, labels = mix_point_clouds_with_colors(pc1, pc2, ratio=0.5)
+	for i, csv_name in enumerate(csv_names):
+		name, ext = os.path.splitext(str(csv_name))
+		name = args.start_class + int(name)
+		name = '%06d' % name
 
-    # Visualize the mixed point cloud with Matplotlib
-    visualize_mixed_point_cloud_matplotlib(mixed_pc, labels)
+		if i > args.numof_classes:
+			break
+
+		if ext != ".csv":
+			continue
+
+		os.makedirs(os.path.join(args.save_root, name), exist_ok=True)
+
+		params = np.genfromtxt(args.load_root + "/" + csv_name, dtype=np.str_, delimiter=",")
+		main_generators = ifs_function()
+		main_obj_num = args.iteration * args.ratio
+			
+		for param in params:
+			main_generators.set_param(float(param[0]), float(param[1]),float(param[2]), float(param[3]),
+				float(param[4]), float(param[5]),float(param[6]), float(param[7]),
+				float(param[8]), float(param[9]),float(param[10]), float(param[11]), float(param[12]))
+
+		main_fractal_point = main_generators.calculate(int(main_obj_num))
+		main_fractal_point = min_max(args, main_fractal_point, axis=None)
+		main_fractal_point = centoroid(main_fractal_point)
+		main_point_data = main_fractal_point.transpose()
+		main_pointcloud = open3d.geometry.PointCloud()
+		main_pointcloud.points = open3d.utility.Vector3dVector(main_point_data)
+
+		fractal_weight = 0
+		for j, mix_csv in enumerate(mix_csv_names):
+			padded_fractal_weight= '%04d' % fractal_weight
+			mix_generators = ifs_function()
+			if j == args.numof_instance:
+				break
+			mix_params = np.genfromtxt(args.load_root + "/" + mix_csv, dtype=np.str_, delimiter=",")
+			for mix_param in mix_params:
+					mix_generators.set_param(float(mix_param[0]), float(mix_param[1]),float(mix_param[2]), float(mix_param[3]),
+					float(mix_param[4]), float(mix_param[5]),float(mix_param[6]), float(mix_param[7]),
+					float(mix_param[8]), float(mix_param[9]),float(mix_param[10]), float(mix_param[11]), float(mix_param[12]))
+			mix_obj_num = args.iteration * (1 - args.ratio)
+			mix_fractal_point = mix_generators.calculate(int(mix_obj_num) + 1)
+			mix_fractal_point = min_max(args, mix_fractal_point, axis=None)
+			mix_fractal_point = centoroid(mix_fractal_point)
+			mix_point_data = mix_fractal_point.transpose()
+			mix_pointcloud = open3d.geometry.PointCloud()
+			mix_pointcloud.points = open3d.utility.Vector3dVector(mix_point_data)
+			fractal_point = np.concatenate((main_fractal_point, mix_fractal_point), axis = 1) 
+
+			# min-max normalize
+			# fractal_point = min_max(args, fractal_point, axis=None)
+			# move to center point 
+			fractal_point = centoroid(fractal_point)
+			# search N/A value
+			arr = np.isnan(fractal_point).any(axis=1)
+
+			if arr[1] == False:
+				point_data = fractal_point.transpose()
+				pointcloud = open3d.geometry.PointCloud()
+				pointcloud.points = open3d.utility.Vector3dVector(point_data)
+				if args.visualize == True:
+					fractal_point_pcd = open3d.visualization.draw_geometries([pointcloud])
+				open3d.io.write_point_cloud((args.save_root + "/" + name + "/" + name + "_" + padded_fractal_weight + ".ply"), pointcloud)
+			fractal_weight += 1
+	
+	endtime = time.time()
+	interval = endtime - starttime
+	print("passed time = %dh %dm %ds" % (int(interval/3600),int((interval%3600)/60),int((interval%3600)%60)))
